d2147b92368ff220dd6316118bdf5eecba2948f5
VadimKorobec <_s_e_t__@ukr.net>
2023-01-15 17:43:20 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,24 +5,39 @@ import { ContactForm } from '../ContactsForm/ContactForm';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
+    name: '',
+    numbers: '',
   };
 
-  formSubmitHandler = data => {
-    console.log(data);
-  };
+  // formSubmitHandler = data => {
+  //   console.log(data);
+  // };
 
   addContact = (name, number) => {
-    // console.log(name, number);
-    this.
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name, number }],
+    }));
+  };
+
+  findContact = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
   };
 
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
+        <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         <Filter />
         <ContactList contacts={this.state.contacts} />
