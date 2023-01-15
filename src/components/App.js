bde33fdf7dadf8319855ea5153f78a8e144b12e8
VadimKorobec <_s_e_t__@ukr.net>
2023-01-15 14:30:14 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,27 @@ export class App extends Component {
     number: '',
   };
 
-  handleNameChange = event => {
-    this.setState({ name: event.currentTarget.value });
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({ [name]: value });
   };
 
-  handleNumberChange = event => {
-    this.setState({ number: event.currentTarget.value });
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
   };
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name
             <input
               type="text"
               value={this.state.name}
-              onChange={this.handleNameChange}
+              onChange={this.handleChange}
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -36,16 +39,15 @@ export class App extends Component {
             <input
               type="tel"
               value={this.state.number}
-              onChange={this.handleNumberChange}
+              onChange={this.handleChange}
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
             />
           </label>
+          <button type="submit">Add contact</button>
         </form>
-
-        <button type="submit">Add contact</button>
       </div>
     );
   }
