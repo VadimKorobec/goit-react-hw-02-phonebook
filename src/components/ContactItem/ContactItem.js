f6be2e29a5a33ff46f863d20da6dbe86c84a46c4
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 13:39:58 +0200

    Update ContactItem.js

---
+++ src/components/ContactItem/ContactItem.js
@@ -0,0 +1,10 @@
+export const ContactItem = ({ contact: { name, number } }) => {
+  return (
+    <div>
+      <p>
+        {name} :{number}
+      </p>
+      <button type="button">Delete</button>
+    </div>
+  );
+};
