199c9dd7345882c3fdb62120310253d81701ada0
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 15:41:59 +0200

    Update App.styled.js

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -4,12 +4,6 @@ export const Container = styled.div`
   padding: 15px 30px;
 `;
 
-export const Section = styled.section`
-  &:not(:last-child) {
-    margin-bottom: 15px;
-  }
-`;
-
 export const TitleH1 = styled.h1`
   margin-bottom: 15px;
   font-size: 24px;
