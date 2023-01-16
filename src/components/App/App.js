229736dd68094303fa1bea9e9dada615804a92bf
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 15:04:25 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,9 +21,24 @@ export class App extends Component {
   //   console.log(data);
   // };
 
-  addContact = (name, number) => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+  addContact = ({ name, number }) => {
+    const normalizedFind = name.toLowerCase();
+    const findName = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === normalizedFind
+    );
+    if (findName) {
+      return alert(`${name} is already in contacts.`);
+    }
+
+    const findNumber = this.state.contacts.find(
+      contact => contact.number === number
+    );
+    if (findNumber) {
+      return alert(`This phone number is already in use.`);
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [{ name, number, id: nanoid() }, ...contacts],
     }));
   };
 
@@ -34,11 +49,23 @@ export class App extends Component {
     );
   };
 
+  getContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   deleteContact = contactId => {
-    console.log(contactId);
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
+  };
+
+  handleFilter = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
   };
 
   render() {
