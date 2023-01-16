6142adb10f571715a95b942d9fb48ee179b63564
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 14:35:37 +0200

    Update ContactList.js

--- src/components/ContactsList/ContactList.js
+++ src/components/ContactsList/ContactList.js
@@ -1,13 +1,23 @@
+import PropTypes from 'prop-types';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 
 export const ContactsList = ({ contacts, onDelete }) => {
   return (
     <ul>
-      {contacts.map((contact, idx) => (
-        <li key={idx}>
-          <ContactItem contact={contact} onDelete={onDelete} />
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <ContactItem contact={(name, number)} onDelete={onDelete} />
         </li>
       ))}
     </ul>
   );
+};
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      idx: PropTypes.string.isRequired,
+    })
+  ),
+  onDelete: PropTypes.func.isRequired,
 };
